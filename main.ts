9216dd90af74dc50d872e2468b7d39876757919a
grace2riku <34134188+grace2riku@users.noreply.github.com>
2023-10-27 08:21:53 +0900

    屋内でのデバッグ用途を想定して、電源ON時にボタンが押されていたら送信強度・LED明るさを抑えるようにした。

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
+let led_brightness = 0
+let radio_strength = 0
 radio.setGroup(1)
-radio.setTransmitPower(7)
+if (input.buttonIsPressed(Button.A)) {
+    radio_strength = 1
+    led_brightness = 64
+} else if (input.buttonIsPressed(Button.B)) {
+    radio_strength = 1
+    led_brightness = 64
+} else if (input.buttonIsPressed(Button.AB)) {
+    radio_strength = 1
+    led_brightness = 64
+} else {
+    radio_strength = 7
+    led_brightness = 255
+}
+led.setBrightness(led_brightness)
+radio.setTransmitPower(radio_strength)
 basic.showString("T")
 basic.forever(function () {
     radio.sendString("1")
